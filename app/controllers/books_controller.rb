f3f76bba761f2276e82dd03b7afc0e8459b9a3f2
永井匠 <iageas7@gmail.com>
2018-08-29 19:10:22 +0900

    Add class name

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!, except:[:top]
+
+  def top
+  end
+
+  def create
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
+   if @book.save
+   flash[:success] = "successfully"
+      redirect_to @book
+    else
+   flash[:danger] = @book.errors.full_messages
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:opinion)
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'books#top'
+  root'books#top'
   get '/about', to: 'books#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
