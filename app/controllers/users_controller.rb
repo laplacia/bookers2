5d44ec5416c3aa62146630197c89597e73d73e44
永井匠 <iageas7@gmail.com>
2018-08-30 21:43:04 +0900

    Create book in user_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
     @book = Book.new
+  end
+
+  def create
+      @book.user_id = current_user.id
+      @book = Book.new(book_params)
+   if @book.save
+   flash[:success] = "successfully"
+      redirect_to @book
+    else
+   flash[:danger] = @book.errors.full_messages
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def index
@@ -16,10 +30,21 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user == current_user
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
+  else
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:success] = "successfully"
+    redirect_to books_path
   end
+  end
+
+
+
+
 
   private
   def user_params
